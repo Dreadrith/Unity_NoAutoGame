a0672d900837418702839dc422bb445d22f2cd35
Dreadrith <valorinferno@gmail.com>
2024-05-26 14:38:03 +0300

    Implement AutoUnfocus

--- Editor/NoAutoGame.cs
+++ Editor/NoAutoGame.cs
@@ -3,45 +3,40 @@ using UnityEditor;
 using System.Threading;
 using System.Threading.Tasks;
 using System;
+using System.Linq;
+using System.Reflection;
 
 namespace DreadScripts.NoAutoGame
 {
     internal static class NoAutoGame
     {
-        private const bool isEnabled = true;
-        private const bool closeGameWindow = true;
-
+        #region Constants
         private const string GAME_WINDOW_TYPE_ASSEMBLY_FULL_NAME = "UnityEditor.GameView, UnityEditor, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
-
         private const string GAME_WINDOW_OPEN_SESS_KEY = "NoAutoGameWindowWasOpen";
-        //private const string LAST_WINDOW_SESS_KEY = "NoAutoGameLastWindowAQN";
-        private const int GAME_WINDOW_HANDLE_DELAY = 100;
+        private const string LAST_WINDOWS_SESS_KEY = "NoAutoGameLastWindowsIDs";
 
-        private static System.Type gameWindowType => _gameWindowType ?? (_gameWindowType = System.Type.GetType(GAME_WINDOW_TYPE_ASSEMBLY_FULL_NAME));
-        private static System.Type _gameWindowType;
+        private const string CLOSE_GAME_WINDOW_PREF_KEY = "NoAutoGameCloseGameWindow";
+        private const string UNFOCUS_GAME_WINDOW_PREF_KEY = "NoAutoGameUnfocusGameView";
+        private const int GAME_WINDOW_HANDLE_DELAY = 100;
+        #endregion
+        
+        private static Type _gameWindowType;
+        private static Type gameWindowType => _gameWindowType ?? (_gameWindowType = Type.GetType(GAME_WINDOW_TYPE_ASSEMBLY_FULL_NAME));
+        private static readonly PropertyInfo hasFocusProperty = typeof(EditorWindow).GetProperty("hasFocus", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        
         private static CancellationTokenSource cts;
 
-        //private static EditorWindow lastWindow;
-
-        [InitializeOnLoadMethod]
-        static void Register()
-        {
-            if (!isEnabled) return;
-            EditorApplication.playModeStateChanged -= HandlePlaymodeChange;
-            EditorApplication.playModeStateChanged += HandlePlaymodeChange;
-
-            /*EditorApplication.update -= RememberCurrentWindow;
-            EditorApplication.update += RememberCurrentWindow;*/
-        }
-
-        static void HandlePlaymodeChange(PlayModeStateChange change)
+        private static bool closeGameWindow;
+        private static bool unfocusGameWindow;
+        private static EditorWindow[] previouslyFocusedWindows;
+        private static void HandlePlaymodeChange(PlayModeStateChange change)
         {
             cts?.Cancel();
 
             switch (change)
             {
                 case PlayModeStateChange.ExitingEditMode:
-                    RememberGameWindow();
+                    RememberWindows();
                     break;
                 case PlayModeStateChange.EnteredPlayMode:
                     try
@@ -51,99 +46,106 @@ namespace DreadScripts.NoAutoGame
                     }
                     catch(OperationCanceledException){}
                     break;
-
-
-                /*case PlayModeStateChange.ExitingPlayMode:
-                    SessionState.EraseString(LAST_WINDOW_SESS_KEY);
-                    break;*/
+                case PlayModeStateChange.EnteredEditMode:
+                    SessionState.EraseString(LAST_WINDOWS_SESS_KEY);
+                    if (!closeGameWindow && unfocusGameWindow && !SessionState.GetBool(GAME_WINDOW_OPEN_SESS_KEY, true))
+                    {
+                        var gameWindows = (EditorWindow[])Resources.FindObjectsOfTypeAll(gameWindowType);
+                        if (gameWindows.Any() && !gameWindows.Any(w => previouslyFocusedWindows.Contains(w)))
+                            foreach(var w in gameWindows)
+                                w.Close();
+                    }
+                        
+                    previouslyFocusedWindows = null;
+                    break;
             }
         }
 
-        static void RememberGameWindow()
+        private static void RememberWindows()
         {
-            var t = Type.GetType(GAME_WINDOW_TYPE_ASSEMBLY_FULL_NAME);
-            if (t == null) return;
-            SessionState.SetBool(GAME_WINDOW_OPEN_SESS_KEY, Resources.FindObjectsOfTypeAll(t).Length > 0);
-        }
+            if (unfocusGameWindow && hasFocusProperty != null)
+            {
+                var idList = string.Join(",", Resources.FindObjectsOfTypeAll<EditorWindow>()
+                                                       .Where(w => (bool) hasFocusProperty.GetValue(w))
+                                                       .Select(w => w.GetInstanceID().ToString()));
+                
+                SessionState.SetString(LAST_WINDOWS_SESS_KEY, idList);
+            }
+            
+            if (gameWindowType == null) return;
+            var gameWindows = Resources.FindObjectsOfTypeAll(gameWindowType);
+            SessionState.SetBool(GAME_WINDOW_OPEN_SESS_KEY, gameWindows.Length > 0); }
 
-        static async Task HandleGameWindow(CancellationToken ct)
+        private static async Task HandleGameWindow(CancellationToken ct)
         {
+            if (!closeGameWindow && !unfocusGameWindow) return;
             var gameWindowWasOpen = SessionState.GetBool(GAME_WINDOW_OPEN_SESS_KEY, true);
-            if (gameWindowWasOpen) return;
             await Task.Delay(GAME_WINDOW_HANDLE_DELAY, ct);
 
-            if (closeGameWindow)
+            if (closeGameWindow && !gameWindowWasOpen)
             {
                 var windows = Resources.FindObjectsOfTypeAll(gameWindowType);
                 foreach (var w in windows)
                 {
-                    var ew = w as EditorWindow;
-                    ew?.Close();
+                    var ew = (EditorWindow)w;
+                    ew.Close();
                 }
-            }
-
-            //Attempt to remember the last focused window and load it when in play mode
-            //This worked if last focused window was docked next to game view, otherwise it didn't
-            /*else
+            } else if (unfocusGameWindow)
             {
-
-                var sessValue = SessionState.GetString(LAST_WINDOW_SESS_KEY, string.Empty);
-                if (sessValue != string.Empty)
-                {
-                    Debug.Log("LOADING");
-                    if (int.TryParse(sessValue[0].ToString(), out int i))
-                    {
-                        Debug.Log(i);
-                        var AQN = sessValue.Substring(1, sessValue.Length - 1);
-                        var t = System.Type.GetType(AQN);
-                        if (t != null)
-                        {
-                            var allWindows = Resources.FindObjectsOfTypeAll(t);
-                            if (i < allWindows.Length)
-                            {
-                                var w = allWindows[i] as EditorWindow;
-                                w?.Focus();
-                            }
-                        }
-                    }
-                }
-            }*/
-        }
-
-        /*static void RememberCurrentWindow()
-        {
-            var w = EditorWindow.focusedWindow;
-            if (w == null || w == lastWindow) return;
-            lastWindow = w;
-
-            var AQN = w.GetType().AssemblyQualifiedName;
-            if (AQN == GAME_WINDOW_TYPE_ASSEMBLY_FULL_NAME) return;
-
-            var t = System.Type.GetType(AQN);
-            if (t != null)
-            {
-                var allWindows = Resources.FindObjectsOfTypeAll(t);
-                var i = allWindows.GetIndexOf(w2 => w2 == w);
-                if (i != -1)
-                    SessionState.SetString(LAST_WINDOW_SESS_KEY, $"{i}{AQN}");
-
+                var previousWindowsFocusList = SessionState.GetString(LAST_WINDOWS_SESS_KEY, string.Empty);
+                if (string.IsNullOrWhiteSpace(previousWindowsFocusList)) return;
+                
+                previouslyFocusedWindows = previousWindowsFocusList
+                                           .Split(',').Select(int.Parse)
+                                           .Select(EditorUtility.InstanceIDToObject)
+                                           .OfType<EditorWindow>().Where(w => w != null).ToArray();
+                foreach (var w in previouslyFocusedWindows)
+                    w.Focus();
             }
         }
+        
 
-        internal static int GetIndexOf<T>(this IEnumerable<T> collection, System.Func<T, bool> predicate)
+        #region Initialization
+        [InitializeOnLoadMethod]
+        private static void Initialize() => LoadVariables();
+        private static void LoadVariables()
         {
-            int index = -1;
-            using (var enumerator = collection.GetEnumerator())
-                while (enumerator.MoveNext())
-                {
-                    checked { ++index; }
-                    if (enumerator.Current == null) continue;
+            closeGameWindow = EditorPrefs.GetBool(CLOSE_GAME_WINDOW_PREF_KEY, false);
+            unfocusGameWindow = EditorPrefs.GetBool(UNFOCUS_GAME_WINDOW_PREF_KEY, true);
+            RefreshHook();
+        }
 
-                    if (predicate(enumerator.Current))
-                        return index;
-                }
-            return -1;
-        }*/
+        private static void RefreshHook()
+        {
+            EditorApplication.playModeStateChanged -= HandlePlaymodeChange;
+            if (closeGameWindow || unfocusGameWindow)
+                EditorApplication.playModeStateChanged += HandlePlaymodeChange;
+        }
+        #endregion
+        
+        #region Menu Items
+        [MenuItem("DreadTools/Utility/NoAutoGame/AutoUnfocus")]
+        private static void ToggleUnfocusGameWindow()
+        {
+            bool newValue = !EditorPrefs.GetBool(UNFOCUS_GAME_WINDOW_PREF_KEY, true);
+            EditorPrefs.SetBool(UNFOCUS_GAME_WINDOW_PREF_KEY, newValue);
+            Debug.Log($"[NoAutoGame] AutoUnfocus is now {(newValue ? "enabled" : "disabled")}.");
+            unfocusGameWindow = newValue;
+            RefreshHook();
+        }
+        
+        [MenuItem("DreadTools/Utility/NoAutoGame/AutoClose")]
+        private static void ToggleCloseGameWindow()
+        {
+            bool newValue = !EditorPrefs.GetBool(CLOSE_GAME_WINDOW_PREF_KEY, false);
+            EditorPrefs.SetBool(CLOSE_GAME_WINDOW_PREF_KEY, newValue);
+            Debug.Log($"[NoAutoGame] AutoClose is now {(newValue ? "enabled" : "disabled")}.");
+            closeGameWindow = newValue;
+            RefreshHook();
+        }
+        #endregion
 
     }
+    
+    
 }
